b5f5cb6ae13b7b3bc46f7c61fec3832d6642bcd9
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-02-14 15:05:39 +0800

    5.2. An Example Program Using Structs

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -19,3 +19,12 @@ fn main() {
     }
     println!("End count = {count}");
 }
+
+fn add_big_strings(dst: &mut Vec<String>, src: &[String]) {
+    let largest: &String = dst.iter().max_by_key(|s| s.len()).unwrap();
+    for s in src {
+        if s.len() > largest.len() {
+            dst.push(s.clone());
+        }
+    }
+}
