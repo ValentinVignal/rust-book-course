03a763fc7e5381dce59ec961ac112a2de201f18d
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-02-21 14:45:04 +0800

    10.2. Traits: Defining Shared Behavior

--- generic/src/main.rs
+++ generic/src/main.rs
@@ -1,22 +1,14 @@
-struct Point<X1, Y1> {
-    x: X1,
-    y: Y1,
-}
-
-impl<X1, Y1> Point<X1, Y1> {
-    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
-        Point {
-            x: self.x,
-            y: other.y,
-        }
-    }
-}
+use aggregator::{Summary, Tweet};
 
 fn main() {
-    let p1 = Point { x: 5, y: 10.4 };
-    let p2 = Point { x: "Hello", y: 'c' };
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
 
-    let p3 = p1.mixup(p2);
-
-    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+    println!("1 new tweet: {}", tweet.summarize());
 }
