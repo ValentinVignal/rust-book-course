b5f8f71aff38229f2b67f2150c2b9b09b34371a2
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-02-21 15:06:23 +0800

    10.3. Validating References with Lifetimes

--- generic/src/main.rs
+++ generic/src/main.rs
@@ -1,14 +1,29 @@
-use aggregator::{Summary, Tweet};
-
 fn main() {
-    let tweet = Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from(
-            "of course, as you probably already know, people",
-        ),
-        reply: false,
-        retweet: false,
-    };
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
 
-    println!("1 new tweet: {}", tweet.summarize());
+    let result = longest_with_an_announcement(
+        string1.as_str(),
+        string2,
+        "Today is someone's birthday!",
+    );
+    println!("The longest string is {}", result);
+}
+
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
